486bb52da61718f71abc82ad5dd7b4444364fa3d
rogerrodriguez27 <roger.rodriguez.27@unsch.edu.pe>
2025-09-15 17:37:06 -0500

    1er commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Importamos express
+const express = require('express');
+const app = express();
+// Definimos un puerto
+const PORT = 3000;
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Hola Mundo desde Express!');
+});
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
